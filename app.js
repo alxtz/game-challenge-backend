5789ea9b3f555b031618a6de00c1dc4f93879275
Alex Tzeng <alxtz.tw@gmail.com>
2017-11-12 20:18:44 +0800

    finish the api

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors');
 const app = express()
 
-app.post('send')
+app.use(cors())
+app.use(bodyParser.json())
+
+const sender = require('./send-sms')
+
+app.post('/send', (reqs, resp) => {
+  console.log('some one wants to send with', reqs.body)
+  sender.sendMessage(reqs.body)
+  resp.send('send it!\n')
+})
+
+app.listen(3000, () => console.log('server listening on localhost:3000'))
--- send-sms.js
+++ send-sms.js
@@ -1,17 +1,23 @@
 const request = require('request')
 
-const postJson = {
-  'api_key': '6852454f',
-  'api_secret': '51313ad58fceb7ca',
-  'to': '886911199304',
-  'from': 'alxtz',
-  'text': 'test the API'
+function sendMessage(useData) {
+  const postJson = {
+    'api_key': '6852454f',
+    'api_secret': '51313ad58fceb7ca',
+    'to': '886911199304',
+    'from': 'alxtz',
+    'text': `Play the game ${useData.gameName} !\nURL: \n${useData.gamePageURL}\n`
+  }
+
+  request({
+    url: 'https://rest.nexmo.com/sms/json',
+    method: "POST",
+    json: postJson
+  }, (err, resp, body) => {
+    console.log('send api success')
+  })
 }
 
-request({
-  url: 'https://rest.nexmo.com/sms/json',
-  method: "POST",
-  json: postJson
-}, (err, resp, body) => {
-  console.log(resp.body)
-})
+module.exports = {
+  sendMessage
+}
